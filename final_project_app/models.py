42aac20df9f7af38fb8a4842f7ea5bc86781be27
karrambs <karra.sogor@gmail.com>
2019-12-03 14:30:24 -0800

    adds models

--- final_project_app/models.py
+++ final_project_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    user_name = models.CharField(max_length=24)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=24)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user_name
+
+class Articles(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    userId = models.ForeignKey()
+
+    def __str__(self):
+        return self.title
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    userId = models.ForeignKey()
+
+    def __str__(self):
+        return self.title
+
