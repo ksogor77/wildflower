2b50362b5eb6b98aa84bd0e6c0ade701cb694dde
karrambs <karra.sogor@gmail.com>
2019-12-10 16:53:42 -0800

    moves around static files

--- final_project/settings.py
+++ final_project/settings.py
@@ -121,12 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = [os.path.join(PROJECT_ROOT, 'static')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
